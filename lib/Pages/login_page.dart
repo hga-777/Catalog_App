2fc27c09e325d4c0210e37e52b4d80e6c138ca45
Hritik Anand <realhga@gmail.com:--global user.email>
2023-06-04 08:42:37 +0530

    Login page is designed

--- lib/Pages/login_page.dart
+++ lib/Pages/login_page.dart
@@ -1,19 +1,63 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_catalog/Pages/homepage.dart';
 
 class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return const Material(
-      child: Center(
-        child: Text(
-          'Login_Page',
-          style: TextStyle(
-            fontSize: 20,
-            fontWeight: FontWeight.bold,
-            color: Color.fromARGB(255, 125, 71, 250),
-          ),
-        ),
-      ),
-    );
+    return Material(
+        color: Colors.white,
+        child: Column(
+          children: [
+            Image.asset(
+              "assets/images/login_image.png", //to add image (downloaded from undraw)
+              fit: BoxFit.cover,
+            ),
+            SizedBox(
+              height: 20.0,
+            ), //ek blank space
+            const Text(
+              "Welcome!!",
+              style: TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                //column is wrapped with padding
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                      //isko column me wrapped kiye
+                      hintText:
+                          "Enter Username", //yeh tb dikhega jb mera user us field ko click kr dega
+                      labelText: "Username", //yeh waise hi dikhte rahega
+                    ),
+                  ),
+                  TextFormField(
+                    obscureText:
+                        true, //this will make the texts in this field as doted
+                    decoration: const InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Pasword",
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  ElevatedButton(
+                    onPressed: () => print('Ha'),
+                    child: Text("Login"),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_catalog/Pages/login_page.dart';
 import 'Pages/homepage.dart';
+import 'package:google_fonts/google_fonts.dart'; //added to use google fonts (pub.dev)
 
 void main() {
   runApp(MyApp());
@@ -13,13 +14,15 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData(
-        primarySwatch: Colors.blueGrey,
-      ), //primary Swatch is the color of appbar , button etc
+        primarySwatch: Colors.purple, //primary Swatch is the color of appbar , button etc
+        fontFamily: GoogleFonts.roboto()
+            .fontFamily, // yeha change krne se saare jagah chnage ho jaega
+      ),
       darkTheme: ThemeData(
         //dark theme property when the dark mode is on
         brightness: Brightness.dark,
       ),
-      initialRoute: "/home", //intial strting page
+      initialRoute: "/login", //intial strting page
       routes: {
         //yeh rasta batata h ki is page ke baad kon sa page jaega
         "/": (context) => LoginPage(), //yeh starting address

644b97fb764ef1a25f33ff22d4d5cdafca443c18
Hritik Anand <realhga@gmail.com:--global user.email>
2023-06-09 11:37:21 +0530

    cart model file has been created

--- lib/Pages/cart_page.dart
+++ lib/Pages/cart_page.dart
@@ -5,46 +5,47 @@ import 'package:flutter/material.dart';
 import 'package:flutter_catalog/widgets/theme.dart';
 import 'package:velocity_x/velocity_x.dart';
 
-
-
 class CartPage extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: context.canvasColor,
-      appBar: AppBar(backgroundColor: context.canvasColor,
-      title: Text("Cart"),),
+      appBar: AppBar(
+        backgroundColor: context.canvasColor,
+        title: Text("Cart"),
+      ),
       body: Column(children: [
-       _CartList().p32().expand(),  //placeholder ek widget h jo ki bta rha ek space liya gya h and expand krne se saara available space le rakha h
-       Divider(),
-       _CartTotal(),
-        
+        _CartList()
+            .p32()
+            .expand(), //placeholder ek widget h jo ki bta rha ek space liya gya h and expand krne se saara available space le rakha h
+        Divider(),
+        _CartTotal(),
       ]),
     );
   }
 }
 
 class _CartTotal extends StatelessWidget {
-
   @override
   Widget build(BuildContext context) {
-    return SizedBox(    //yeh krne se hmlog height ,width aaram se add kr sakte h
+    return SizedBox(
+      //yeh krne se hmlog height ,width aaram se add kr sakte h
       height: 200,
-      child: Row(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        children: [
-       "\$999".text.xl5.color(Theme.of(context).colorScheme.secondary).make(),
+      child: Row(mainAxisAlignment: MainAxisAlignment.spaceAround, children: [
+        "\$999".text.xl5.color(Theme.of(context).colorScheme.secondary).make(),
         30.widthBox,
-       ElevatedButton(onPressed: (){}, 
-       style: ButtonStyle(
-        backgroundColor: 
-        MaterialStateProperty.all(MyTheme.darkBluish),
-
-       ),
-       child: "Buy".text.white.make(),).w32(context),
+        ElevatedButton(
+          onPressed: () {
+            ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+              content: "Buying not supported yet.".text.make(),
+            ));
+          },
+          style: ButtonStyle(
+            backgroundColor: MaterialStateProperty.all(MyTheme.darkBluish),
+          ),
+          child: "Buy".text.white.make(),
+        ).w32(context),
       ]),
-
     );
   }
 }
---
+++ lib/models/cart.dart
@@ -0,0 +1,39 @@
+import 'package:flutter_catalog/models/catalog.dart';
+
+class CartModel {
+  // catalog field
+  CatalogModel _catalog = CatalogModel();   //late CatalogModel _catalog;
+
+
+
+  // Collection of IDs - store Ids of each item
+  final List<int> _itemIds = [];
+
+  // Get Catalog
+  CatalogModel get catalog => _catalog;
+
+  set catalog(CatalogModel newCatalog) {
+    assert(newCatalog != null);
+    _catalog = newCatalog;
+  }
+
+  // Get items in the cart
+List<Items> get items =>
+    _itemIds.map((id) => CatalogModel.getByID(id)).toList();
+
+  // Get total price
+  num get totalPrice =>
+      items.fold(0, (total, current) => total + current.price);
+
+  // Add Item
+
+  void add(Items item) {
+    _itemIds.add(item.id.toInt());
+  }
+
+  // Remove Item
+
+  void remove(Items item) {
+    _itemIds.remove(item.id);
+  }
+}
